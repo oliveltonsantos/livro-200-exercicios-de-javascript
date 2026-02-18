dc6a516e3eaf8dbfa6337d977f3ebc9b1d79f84d
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2026-02-18 12:25:49 -0300

    ex 42

--- exercicio-041/2-codigo.js
+++ exercicio-041/2-codigo.js
@@ -3,6 +3,7 @@ const btnConverterBinario = document.querySelector('.btnConverterBinario')
 const btnNovaConversao = document.querySelector('.btnNovaConversao')
 const mensagem = document.querySelector('.mensagem')
 
+
 function converterBinario() {
     if (inputNumero.value === '') {
         alert('Digite um número para converter.')
@@ -10,32 +11,51 @@ function converterBinario() {
         return
     }
 
-    let numero = Number(inputNumero.value)
+    const numeroOriginal = Number(inputNumero.value)
 
-    let restoDaDivisao = 0
-    let numeroBinario = []
- 
+
+    let numeroCopia = numeroOriginal
+    const numeroBinario = []
+
     do {
-        restoDaDivisao = numero % 2
-        
-    if (restoDaDivisao === 0) {
+        if (numeroCopia % 2 === 0) {
             numeroBinario.push(0)
         } else {
             numeroBinario.push(1)
         }
 
-        numero = restoDaDivisao
+        const divisao = numeroCopia / 2
 
-    } while (numero !== 0);
+        numeroCopia = Math.floor(divisao)
 
-
-
-    mensagem.innerHTML = `<p>${numeroBinario}</p>`
+    } while (numeroCopia > 0);
 
 
+    let numeroInvertido = ''
+
+    for (
+        let contador = numeroBinario.length - 1;
+        contador >= 0;
+        contador--
+    ) {
+        numeroInvertido += numeroBinario[contador]
+    }
 
 
+    mensagem.innerHTML = `<p>${numeroOriginal} = ${numeroInvertido} (binário)</p>`
 
 
+    btnConverterBinario.style.display = 'none'
+    btnNovaConversao.style.display = 'inline-block'
+}
 
+
+function novaConversao() {
+    inputNumero.value = ''
+    inputNumero.focus()
+
+    btnConverterBinario.style.display = 'inline-block'
+    btnNovaConversao.style.display = 'none'
+
+    mensagem.innerHTML = ''
 }
---
+++ exercicio-042/0-descricao-do-exercicio.md
@@ -0,0 +1,7 @@
+## Descrição do exercício:
+
+Usando um laço for, crie um programa que converte um número binário em um número decimal.
+
+Por exemplo, o número binário 1010 convertido em decimal é 10.
+
+
