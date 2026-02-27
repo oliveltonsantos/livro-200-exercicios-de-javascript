70ec939af1dff061cee95331e4df5123d42725ba
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2026-02-27 11:46:12 -0300

    ex 44 update

--- exercicio-044/2-codigo.js
+++ exercicio-044/2-codigo.js
@@ -2,17 +2,20 @@ const inputNumero = document.getElementById('numero')
 const btnAdicionarNumero = document.querySelector('.btnAdicionarNumero')
 const btnNovaSoma = document.querySelector('.btnNovaSoma')
 const listaNumeros = document.querySelector('.listaNumeros')
-const resultado = document.querySelector('.resultado')
+const mensagem = document.querySelector('.mensagem')
 
 const matrizA = []
 const matrizB = []
+const matrizResultado = []
 
 let linha = 0
 let coluna = 0
 const tamanho = 2
 
-let fase = 'A'
+let fase = 'A' // controle de fase (deve ser global pois é estado do programa e não da função)
+let nomeDaMatriz = 'A'
 let matrizAtual = matrizA
+
 
 function adicionarNumero() {
 
@@ -29,13 +32,41 @@ function adicionarNumero() {
     if (coluna === tamanho) {
         coluna = 0
         linha++
-
     }
+
+    inputNumero.value = ''
+    inputNumero.focus()
+
+    mensagem.innerHTML =
+        `<p>Matriz ${nomeDaMatriz}:</p>` + 
+        matrizAtual
+            .map(linha => `[${linha.join(' , ')}]`)
+            .join('<br>')
+
 
     if (linha === tamanho && fase === 'A') {
-        resultado.innerHTML = `${matrizAtual}`
+        fase = 'B'
+        matrizAtual = matrizB
+        nomeDaMatriz = 'B'
+        linha = 0
+        coluna = 0
+    } else if (linha === tamanho && fase === 'B') {
+        
+        let l = 0
+
+        do {
+            matrizResultado[l] = []
+
+            for (let j = 0; j < tamanho; j++) {
+                matrizResultado[l][j] = matrizA[l][j] + matrizB[l][j]
+            }
+            l++
+        } while (l < tamanho)
+
+        mensagem.innerHTML = `
+            <p>${matrizResultado}</p>
+        `
     }
 
-}
-
-
+    
+}  
