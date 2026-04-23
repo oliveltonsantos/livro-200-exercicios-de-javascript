ca6b5874e85631e9000bca43111d5db996a542ec
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2026-04-23 20:03:36 -0300

    ex 85

--- exercicio-085/0-descricao-do-exercicio.md
+++ exercicio-085/0-descricao-do-exercicio.md
@@ -2,7 +2,7 @@
 
 Neste exercício, você precisa criar um objeto que represente um quadrado. 
 
-Este objeto deve herdar as   retangulo criado no exercício anterior. 
+Este objeto deve herdar as propriedades do objeto retangulo criado no exercício anterior. 
 
 Em seguida, substitua o método para calcular a área para que utilize apenas uma das dimensões (já que a altura e a largura de um quadrado são iguais).
 
--- exercicio-085/2-codigo.js
+++ exercicio-085/2-codigo.js
@@ -1,17 +1,68 @@
-
-
-
+const inputAltura = document.getElementById('altura')
+const inputLargura = document.getElementById('largura')
+const todosInputs = document.querySelectorAll('input')
+const btnDadosDoRetangulo = document.querySelector('.btnDadosDoRetangulo')
+const btnNovoCalculo = document.querySelector('.btnNovoCalculo')
+const mensagem = document.querySelector('.mensagem')
 
 
 // Interação com usuário
+function dadosDoRetangulo() {
+    const inputVazio = [...todosInputs].find(input => input.value === '')
 
+    if (inputVazio) {
+        alert('Preencha todos os campos.')
+        inputVazio.focus()
+        return
+    }
 
+    const altura = Number(inputAltura.value)
+    const largura = Number(inputLargura.value)
 
+    const retangulo = {
+        altura: altura,
+        largura: largura,
 
+        areaDoRetangulo() {
+            return this.altura * this.largura
+        }
+    }
+
+    // Herança das propriedades do retangulo 
+    const quadrado = Object.create(retangulo)
+
+    // Substituindo o método para calcular a área 
+    quadrado.areaDoRetangulo = function() {
+        return this.largura * this.largura
+    }
+
+    
+    mensagem.innerHTML = `
+        <p>Área retangulo = ${retangulo.areaDoRetangulo()}</p>
+        <p>Área quadrado = ${quadrado.areaDoRetangulo()}</p>
+    `
+
+    todosInputs.forEach(input => input.disabled = true)
+
+    btnDadosDoRetangulo.style.display = 'none'
+    btnNovoCalculo.style.display = 'inline-block'
+}
 
 
 // Reset
+function novoCalculo() {
+    todosInputs.forEach(input => {
+        input.disabled = false
+        input.value = ''
+    })
 
+    inputAltura.focus()
+
+    btnDadosDoRetangulo.style.display = 'inline-block'
+    btnNovoCalculo.style.display = 'none'
+
+    mensagem.innerHTML = ''
+}
 
 
 
