f2052bb5a90943966ec730f67ce100e72bfa8e73
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2026-05-08 20:01:35 -0300

    ex 93

--- exercicio-093/2-codigo.js
+++ exercicio-093/2-codigo.js
@@ -1,6 +1,43 @@
+const inputNumero = document.getElementById('numero')
+const btnAdicionarNumero = document.querySelector('.btnAdicionarNumero')
+const btnProcessarValores  = document.querySelector('.btnProcessarValores')
+const btnNovaLista = document.querySelector('.btnNovaLista')
+const mensagem = document.querySelector('.mensagem')
+
+
+const listaNumeros = []
+
+// Função de alta ordem
+function calcularMediana(lista) {
+
+
+
+}
+
 
 
 // Interação com usuário
+function adicionarNumero() {
+    if (inputNumero.value === '') {
+        alert('Digite um número para adicionar.')
+        inputNumero.focus()
+        return
+    }
+
+    const numero = Number(inputNumero.value)
+
+    listaNumeros.push(numero)
+
+    mensagem.innerHTML = `<p>Números adicionados: ${listaNumeros.join(' > ')}</p>`
+
+    inputNumero.value = ''
+    inputNumero.focus()
+}
+
+
+function processarValores() {
+    mensagem.innerHTML = `<p>${calcularMediana(listaNumeros)}</p>`
+}
 
 
 // Reset
