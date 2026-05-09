e427b49eb0b5dc60123d56683d8699e74a4446b8
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2026-05-09 20:08:17 -0300

    ex 93

--- exercicio-093/2-codigo.js
+++ exercicio-093/2-codigo.js
@@ -1,19 +1,50 @@
 const inputNumero = document.getElementById('numero')
 const btnAdicionarNumero = document.querySelector('.btnAdicionarNumero')
-const btnProcessarValores  = document.querySelector('.btnProcessarValores')
+const btnProcessarValores = document.querySelector('.btnProcessarValores')
 const btnNovaLista = document.querySelector('.btnNovaLista')
 const mensagem = document.querySelector('.mensagem')
 
 
 const listaNumeros = []
 
+
 // Função de alta ordem
 function calcularMediana(lista) {
 
+    for (let i = 0; i < lista.length; i++) {
 
+        for (let j = 0; j < lista.length - 1; j++) {
+
+            if (lista[j] > lista[j + 1]) {
+                let temporario = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = temporario
+            }
+        }
+    }
+
+    if (lista.length % 2 !== 0) {
+        const posicao = (lista.length + 1) / 2
+        return lista[posicao]
+    } else {
+        const posicao1 = lista.length / 2
+        const posicao2 = (lista.length / 2) + 1
+        const media = (lista[posicao1] + lista[posicao2]) / 2
+        return media
+    }
 
 }
 
+
+function processarValores() {
+    mensagem.innerHTML = `<p>Mediana: ${calcularMediana(listaNumeros)}</p>`
+
+    inputNumero.disabled = true
+
+    btnAdicionarNumero.style.display = 'none'
+    btnProcessarValores.style.display = 'none'
+    btnNovaLista.style.display = 'inline-block'
+}
 
 
 // Interação com usuário
@@ -35,11 +66,16 @@ function adicionarNumero() {
 }
 
 
-function processarValores() {
-    mensagem.innerHTML = `<p>${calcularMediana(listaNumeros)}</p>`
-}
-
-
 // Reset
+function novaLista() {
+    listaNumeros.length = 0
 
+    inputNumero.disabled = false
+    inputNumero.focus()
 
+    btnAdicionarNumero.style.display = 'inline-block'
+    btnProcessarValores.style.display = 'inline-block'
+    btnNovaLista.style.display = 'none'
+
+    mensagem.innerHTML = ''
+}
